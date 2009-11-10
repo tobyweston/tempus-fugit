977740fd26fd0f147ad9234d5b0486c847a04a2a
Toby Weston <toby.weston@gmail.com>
2009-11-10 18:13:04 +0000

    added completion service stuff and executor shutdown wotsit, updated pom to source level 1.5

---
+++ tempus-fugit/src/main/java/org/tempus/fugit/concurrency/DefaultTimeoutableCompletionService.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2009, Toby Weston 
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+package org.tempus.fugit.concurrency;
+
+import static org.tempus.fugit.concurrency.Interrupter.interrupt;
+import org.tempus.fugit.temporal.DateFactory;
+import org.tempus.fugit.temporal.DefaultDateFactory;
+import org.tempus.fugit.temporal.Duration;
+import static org.tempus.fugit.temporal.Duration.seconds;
+
+import static java.lang.Thread.currentThread;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.*;
+
+public class DefaultTimeoutableCompletionService implements TimeoutableCompletionService {
+
+    private static final boolean INTERRUPT_IF_RUNNING = true;
+    private static final Duration DEFAULT_TIMEOUT = seconds(30);
+
+    private final java.util.concurrent.CompletionService completionService;
+    private final Duration timeout;
+    private final DateFactory time;
+
+    public DefaultTimeoutableCompletionService(CompletionService completionService) {
+        this(completionService, DEFAULT_TIMEOUT, new DefaultDateFactory());
+    }
+
+    public DefaultTimeoutableCompletionService(CompletionService completionService, Duration timeout, DateFactory time) {
+        this.timeout = timeout;
+        this.time = time;
+        this.completionService = completionService;
+    }
+
+    public <T> List<T> submit(List<? extends java.util.concurrent.Callable<T>> tasks) throws ExecutionException, TimeoutException {
+        List<Future<T>> submitted = new ArrayList<Future<T>>();
+        try {
+            for (Callable task : tasks) {
+                submitted.add(completionService.submit(task));
+            }
+            return waitFor(tasks.size(), timeout);
+        } finally {
+            for (Future<T> future : submitted) {
+                future.cancel(INTERRUPT_IF_RUNNING);
+            }
+        }
+    }
+
+    private <T> List<T> waitFor(int tasks, Duration timeout) throws ExecutionException, TimeoutException {
+        List<T> completed = new ArrayList<T>();
+        Interrupter interrupter = interrupt(currentThread()).using(time).after(timeout);
+        try {
+            for (int i = 0; i < tasks; i++) {
+                try {
+                    Future<T> future = completionService.take();
+                    completed.add(future.get());
+                } catch (InterruptedException e) {
+                    throw new TimeoutExceptionWithFutures("timed out after " + timeout.toString(), completed);
+                }
+            }
+        } finally {
+            interrupter.cancel();
+        }
+        return completed;
+    }
+}
