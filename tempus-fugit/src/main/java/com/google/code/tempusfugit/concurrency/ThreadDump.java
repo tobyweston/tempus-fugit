58ac58d6ce16452f2c9d47a5829fb8e579903600
Toby Weston <toby.weston@gmail.com>
2009-12-08 15:56:42 +0000

    toby - added basic thread dump class for dumping stack traces for all threads

---
+++ tempus-fugit/src/main/java/com/google/code/tempusfugit/concurrency/ThreadDump.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2009, tempus-fugit committers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.code.tempusfugit.concurrency;
+
+import java.io.PrintStream;
+import java.util.Map;
+
+public class ThreadDump {
+
+    public static void dumpThreads(PrintStream writer) {
+        Map<Thread, StackTraceElement[]> traces = Thread.getAllStackTraces();
+        for (Thread thread : traces.keySet()) {
+            writer.println(String.format("\nThread %s@%d: (state = %s)", thread.getName(), thread.getId(), thread.getState()));
+            for (StackTraceElement stackTraceElement : traces.get(thread)) {
+                writer.println(" - " + stackTraceElement);
+            }
+        }
+    }
+
+}
---
+++ tempus-fugit/src/test/java/com/google/code/tempusfugit/concurrency/ThreadDumpTest.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2009, tempus-fugit committers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.code.tempusfugit.concurrency;
+
+import org.jmock.Expectations;
+import org.jmock.Mockery;
+import org.jmock.integration.junit4.JMock;
+import org.jmock.lib.legacy.ClassImposteriser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.PrintStream;
+
+@RunWith(JMock.class)
+public class ThreadDumpTest {
+
+    private final Mockery context = new Mockery() {{
+        setImposteriser(ClassImposteriser.INSTANCE);
+    }};
+
+    private PrintStream stream = context.mock(PrintStream.class);
+    private static final String EXPECTED_THREAD_DETAILS_LINE = "\nThread main@1: (state = RUNNABLE)";
+
+    @Test
+    public void outputsThreadDetails() {
+        context.checking(new Expectations() {{
+            one(stream).println(EXPECTED_THREAD_DETAILS_LINE);
+            ignoring(stream).println(with(any(String.class)));
+        }});
+        ThreadDump.dumpThreads(stream);
+    }
+
+    @Test
+    public void outputsThreadStackTraceDetails() {
+        context.checking(new Expectations() {{
+            ignoring(stream).println(EXPECTED_THREAD_DETAILS_LINE);
+            one(stream).println(" - java.lang.Thread.dumpThreads(Native Method)");
+            allowing(stream).println(with(any(String.class)));
+        }});
+        ThreadDump.dumpThreads(stream);
+    }
+    
+}
