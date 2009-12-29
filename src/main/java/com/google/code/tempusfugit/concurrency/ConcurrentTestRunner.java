1eda21b97615aabc9e504181d8183d3cdcf1cfe2
Toby Weston <toby.weston@gmail.com>
2009-12-29 18:57:16 +0000

    toby - added ConcurrentTestRunner to run tests in parallel within a test class, added documentation to site too

---
+++ src/main/java/com/google/code/tempusfugit/concurrency/ConcurrentTestRunner.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2009, tempus-fugit committers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.code.tempusfugit.concurrency;
+
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.RunnerScheduler;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static com.google.code.tempusfugit.concurrency.ExecutorServiceShutdown.shutdown;
+import static com.google.code.tempusfugit.temporal.Duration.seconds;
+import static java.util.concurrent.Executors.newCachedThreadPool;
+
+public class ConcurrentTestRunner extends BlockJUnit4ClassRunner {
+
+    public ConcurrentTestRunner(Class<?> type) throws InitializationError {
+        super(type);
+        setScheduler(new ConcurrentScheduler());
+    }
+
+    private static class ConcurrentScheduler implements RunnerScheduler {
+
+        private ExecutorService executor;
+
+        public ConcurrentScheduler() {
+            executor = newCachedThreadPool(new ThreadFactory() {
+                private AtomicLong count = new AtomicLong();
+                public Thread newThread(Runnable runnable) {
+                    return new Thread(runnable, "ConcurrentTestRunner-Thread-" + count.getAndIncrement());
+                }
+            });
+        }
+
+        public void schedule(Runnable childStatement) {
+            executor.submit(childStatement);
+        }
+
+        public void finished() {
+            shutdown(executor).waitingForCompletion(seconds(10));
+        }
+    }
+
+}
---
+++ src/test/java/com/google/code/tempusfugit/concurrency/ConcurrentTestRunnerTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2009, tempus-fugit committers
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.code.tempusfugit.concurrency;
+
+import junit.framework.AssertionFailedError;
+import org.junit.AfterClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.util.Collections.synchronizedSet;
+import static junit.framework.Assert.fail;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(ConcurrentTestRunner.class)
+public class ConcurrentTestRunnerTest {
+
+    private static final Set<String> threads = synchronizedSet(new HashSet<String>());
+
+    @Test
+    public void shouldRunInParallel1() {
+        add();
+    }
+
+    @Test
+    public void shouldRunInParallel2() {
+        add();
+    }
+
+    @Test
+    public void shouldRunInParallel3() {
+        add();
+    }
+
+    @Test
+    public void shouldRunInParallel4() {
+        add();
+    }
+
+    @Test
+    public void shouldRunInParallel5() {
+        add();
+    }
+
+    private void add() {
+        threads.add(Thread.currentThread().getName());
+    }
+
+    @AfterClass
+    public static void assertTestThreadsSpawned() {
+        assertThat(threads.size(), is(5));
+    }
+
+    @Test(expected = AssertionFailedError.class)
+    public void concurrentFailuresFailInTheMainTestThread() throws InterruptedException {
+        fail();
+    }
+
+}
